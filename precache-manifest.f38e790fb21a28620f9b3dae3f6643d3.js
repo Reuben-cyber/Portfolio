f36646746d1624587072c777d6617f73821cb6e5
Reuben-cyber <reuben2000@outlook.com>
2022-09-05 10:34:11 +0530

    Updates

---
+++ precache-manifest.f38e790fb21a28620f9b3dae3f6643d3.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "86ec5d873e307114473d",
+    "url": "/Portfolio/static/css/main.3ceb4c0a.chunk.css"
+  },
+  {
+    "revision": "86ec5d873e307114473d",
+    "url": "/Portfolio/static/js/main.86ec5d87.chunk.js"
+  },
+  {
+    "revision": "bb0898e875365f106adb",
+    "url": "/Portfolio/static/js/1.bb0898e8.chunk.js"
+  },
+  {
+    "revision": "a772b0ea5c1fc0137646",
+    "url": "/Portfolio/static/js/2.a772b0ea.chunk.js"
+  },
+  {
+    "revision": "82d8c600e0cb3ea7758a",
+    "url": "/Portfolio/static/js/runtime~main.82d8c600.js"
+  },
+  {
+    "revision": "74562b6a4978603db1a8fdd2d555f30a",
+    "url": "/Portfolio/static/media/3.74562b6a.gif"
+  },
+  {
+    "revision": "adec968ca329c81550c8e81a09ca517c",
+    "url": "/Portfolio/static/media/event mgmt.adec968c.jpg"
+  },
+  {
+    "revision": "45bdb29778f1171c46bec4d0a2aced06",
+    "url": "/Portfolio/static/media/disharangaalay.45bdb297.jpg"
+  },
+  {
+    "revision": "d8bc3938faa5a7c48a0c5ba12a1c9d2a",
+    "url": "/Portfolio/static/media/juniberry.d8bc3938.jpg"
+  },
+  {
+    "revision": "f6460c95de0a7ce5cb505a189da18521",
+    "url": "/Portfolio/static/media/event doc.f6460c95.jpg"
+  },
+  {
+    "revision": "2fe99c5ab8293a61d5f20de04c867bf7",
+    "url": "/Portfolio/static/media/artsy.2fe99c5a.jpg"
+  },
+  {
+    "revision": "8c02c59060790748b8ed52be76404be8",
+    "url": "/Portfolio/static/media/students.8c02c590.jpg"
+  },
+  {
+    "revision": "1271239db97f4747590f9b9f88746b6f",
+    "url": "/Portfolio/static/media/pocketscanner.1271239d.jpg"
+  },
+  {
+    "revision": "7cbe134f0b53ed4981fc976af1c7b88b",
+    "url": "/Portfolio/static/media/counter.7cbe134f.jpg"
+  },
+  {
+    "revision": "247a5a3a71d110919731ddce0ca7d6d6",
+    "url": "/Portfolio/static/media/bin.247a5a3a.jpg"
+  },
+  {
+    "revision": "217f67bf57150000dc1eab535c0d6498",
+    "url": "/Portfolio/static/media/stats.217f67bf.JPG"
+  },
+  {
+    "revision": "0d5c89c437430471c345575bac8de0cb",
+    "url": "/Portfolio/static/media/movinture.0d5c89c4.jpg"
+  },
+  {
+    "revision": "b28d7e3805b046d301732b55fd521dff",
+    "url": "/Portfolio/static/media/desgning.b28d7e38.JPG"
+  },
+  {
+    "revision": "cb0c2d21979e8e2dd4dcad4a347a4769",
+    "url": "/Portfolio/static/media/IoT.cb0c2d21.JPG"
+  },
+  {
+    "revision": "53bda3ddc2994fffc0671b7868386844",
+    "url": "/Portfolio/static/media/insta.53bda3dd.png"
+  },
+  {
+    "revision": "c253cc90ead9cbf02a4d3f741e52dc04",
+    "url": "/Portfolio/index.html"
+  }
+];
